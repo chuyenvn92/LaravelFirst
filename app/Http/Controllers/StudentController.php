7c7ed028546f3c262c86759a2a84853ba690a1cb
chuyen <maicongchuyentb@gmail.com>
2020-08-21 14:38:10 +0700

    update data

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -38,4 +38,21 @@ class StudentController extends Controller
         $student = Student::find($id);
         return view('edit',compact('student'));
     }
+
+    public function update(Request $request, $id){
+        $this->validate($request,[
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required',
+            'phone' => 'required'
+        ]);
+
+        $student = Student::find($id);
+        $student->first_name = $request->firstname;
+        $student->last_name = $request->lastname;
+        $student->email = $request->email;
+        $student->phone = $request->phone;
+        $student->save();
+        return redirect(route('home'))->with('successMsg','Student Successfully Updated');
+    }
 }

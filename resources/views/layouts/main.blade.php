eeba6d26fff9e7a1c40dfd7fae46e49042b260a1
chuyen <maicongchuyentb@gmail.com>
2020-08-21 09:51:09 +0700

    config main + add footer-navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,17 +19,11 @@
 <body>
 
   <!-- Start your project here-->
-  <div style="height: 100vh">
-    <div class="flex-center flex-column">
+@include('layouts.navbar')
 
-      <h1 class="text-hide animated fadeIn mb-4"
-        style="background-image: url('https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'); width: 250px; height: 90px;">
-        MDBootstrap</h1>
-      <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
+@yield('content')
 
-      <p class="animated fadeIn text-muted">MDB Team</p>
-    </div>
-  </div>
+@include('layouts.footer')
   <!-- Start your project here-->
 
   <!-- SCRIPTS -->
